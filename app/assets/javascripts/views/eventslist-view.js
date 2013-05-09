1613302343a774eca3831f8f0cbad735f000158e
Bryant Detwiller <bryant@detwiller.com>
2013-05-09 13:57:44 -0700

    fix trending locations

--- app/assets/javascripts/views/eventslist-view.js
+++ app/assets/javascripts/views/eventslist-view.js
@@ -1,29 +1,27 @@
 App.Views.EventsList = Backbone.View.extend({
 
-	addtoSidebar: function() {
+	addtoSidebar: function(venue) {
     var that = this;
-    that.$el.empty();
 
-    that.collection.each(function(venue) {
-    	venue.addMarker();
-      that.$el.append(JST["gallery/venue"]( {venue: venue} ));
+  	venue.addMarker();
+    that.$el.append(JST["gallery/venue"]( {venue: venue} ));
 
-      $('#' + venue.get("id") + " .venue-content").click(function () { 
-        $('#' + venue.get("id") + "_photos").slideToggle();
-      });
-
-      $('#' + venue.get("id") + " .venue-icon").click(function () { 
-        $('#' + venue.get("id") + "_photos").slideToggle();
-      });
-
-      $('#' + venue.get("id")).mouseover(function () { 
-        $('#' + venue.get("id") + " .venue-content").addClass("highlight");
-      });
-
-      $('#' + venue.get("id")).mouseout(function () { 
-        $('#' + venue.get("id") + " .venue-content").removeClass("highlight");
-      });
+    $('#' + venue.get("id") + " .venue-content").click(function () { 
+      $('#' + venue.get("id") + "_photos").slideToggle();
     });
+
+    $('#' + venue.get("id") + " .venue-icon").click(function () { 
+      $('#' + venue.get("id") + "_photos").slideToggle();
+    });
+
+    $('#' + venue.get("id")).mouseover(function () { 
+      $('#' + venue.get("id") + " .venue-content").addClass("highlight");
+    });
+
+    $('#' + venue.get("id")).mouseout(function () { 
+      $('#' + venue.get("id") + " .venue-content").removeClass("highlight");
+    });
+
     
   },
 
