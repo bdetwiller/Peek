811ce6484337aebeadee5ef95cb6a40232b81968
Bryant Detwiller <bryant.detwiller@gmail.com>
2014-05-07 02:39:59 -0700

    removed files

--- config/instagram.rb
+++
@@ -1,9 +0,0 @@
-require "instagram"
-
-
-CALLBACK_URL = "http://localhost:4567/oauth/callback"
-
-Instagram.configure do |config|
-  config.client_id = "c8f7973e4c2a40909deb8ac3c5c11843"
-  config.client_secret = "c02752f7a0d54e32b510859841d67ac5"
-end
